914d22a66c3600b6a4df8f6bc80067d741c7e1ed
Yann Lemos <yanndemoraes@gmail.com>
2026-04-27 09:02:27 -0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
   kit: {
-    // Replace the default adapter with adapter-static
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: '404.html', // Recommended for Single Page App behavior
+      fallback: undefined,
       precompress: false,
       strict: true
     }),
     paths: {
-      // Set to your repo name if not a custom domain or root site
-      base: process.env.NODE_ENV === 'production' ? '/yannlemos' : '',
+      base: process.env.NODE_ENV === 'production' ? '/yannlemos' : ''
     }
   }
 };
