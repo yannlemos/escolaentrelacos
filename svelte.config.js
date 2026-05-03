00ce3105a702c36c529203a38e1fc2d06aaa801b
Yann Lemos <yanndemoraes@gmail.com>
2026-05-03 13:37:34 -0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
       strict: true
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/yannlemos' : ''
+      base: ''
     }
   },
   preprocess: vitePreprocess()
